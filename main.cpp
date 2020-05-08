750f45f95d2a6c72f984c7c67c580d5ae92f794e
Nikita Markelov <marknik139@gmail.com>
2020-05-08 14:30:56 +0300

    code: correct work + design

--- main.cpp
+++ main.cpp
@@ -7,17 +7,17 @@ using namespace std;
 #define Const 100
 
 //Input default values of channels
-void input_channels(double M[], int size)
+void input_channels(int M[], int number_of_channels)
 {
-    for (int i=0; i<size; i++)
+    for (int i=0; i<number_of_channels; i++)
     {
-        cout<<"Define M"<<i+1<<" (Channel "<<i+1<<") >> ";
+        cout<<"Define number of channel for measuring (1-6) >> ";
         cin>>M[i];
     }
 }
 
 //Measuring N values for k channels
-void measure_X(double M[],double X[][Const], int raws, int columns)
+void measure_X(int M[],double X[][Const], int raws, int columns)
 {
     Plant plant;
     plant_init(plant);
@@ -62,32 +62,53 @@ double covariation(double X[][Const], double X_a[], int i, int j, int N)
     return temp;
 }
 
+void name(int ELEM)
+{
+    if (ELEM==1)
+        cout<<"Temperature";
+    if (ELEM==2)
+        cout<<"Pressure";
+    if (ELEM==3)
+        cout<<"Wetness";
+    if (ELEM==4 || ELEM==5 || ELEM==6)
+        cout<<"Discret";
+}
+
 //Output for 2-x matrix
-void output_measure(double X[][Const], int raws, int columns, string text, string text2)
+void output_measure(double X[][Const], int M[], int raws, int columns, string text, string text2)
 {
     cout<<"\n";
     cout<<text<<"\n";
     for (int i=0; i<raws; i++)
     {
-        cout<<i+1<<") Measure \n";
+        cout<<M[i]<<") Measure \n";
+        name(M[i]);
+        cout<<"\n";
         for (int j=0; j<columns; j++)
         {
-            cout<<"\t"<<text2<<"["<<i+1<<"]["<<j+1<<"]="<<X[i][j];
-            cout<<"\n";
+            if (text2=="C")
+            {
+                cout<<"\t"<<text2<<M[i]<<"="<<X[i][j]<<"\n";
+            }
+            else
+            {
+                cout<<"\t"<<text2<<"["<<M[i]<<"]["<<j+1<<"]="<<X[i][j]<<"\n";
+            }
         }
     }
 }
 
 //Output for 1-x matrix
-void output_average(double X[],int size_of_X, string text)
+void output_average(double X[],int M[], int size_of_X, string text)
 {
     cout<<"\n";
     cout<<text<<"\n";
 
     for (int i=0; i<size_of_X; i++)
     {
-        cout<<i+1<<") Average measure";
-        cout<<"\t"<<"X["<<i+1<<"]="<<X[i];
+        cout<<M[i]<<") Average measure of ";
+        name(M[i]);
+        cout<<"\t"<<"X="<<X[i];
         cout<<"\n";
     }
 }
@@ -110,16 +131,16 @@ int main()
         cout<<"Define N (Cycle number) >> ";
         cin>>N;
 
-        double M[k];
+        int M[k];
         input_channels(M, k);
 
         double X[k][Const];
         measure_X(M, X, k, N);
-        output_measure(X,k, N, "Measures of devices:", "X");
+        output_measure(X,M, k,N, "Measures of devices:", "X");
 
         double X_a[k];
         average(X, k, N, X_a);
-        output_average(X_a, k, "Average values of measurements: ");
+        output_average(X_a,M, k, "Average values of measurements: ");
 
         double covar[k][Const];
 
@@ -132,7 +153,7 @@ int main()
             }
         }
 
-        output_measure(covar, k, k, "Covariations:", "C");
+        output_measure(covar, M, k, k, "Covariations:", "C");
 
         cout<<"\n Repeat??? (1-YES; Anything-NO) >> ";
         cin>>continue_programm;
